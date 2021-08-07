cc52207de2d81acabc2de2bfdfe15e52ed15fd4a
afwolfe <3718652+afwolfe@users.noreply.github.com>
2021-08-07 15:06:40 -0400

    Working on UserResource API [42:15]

---
+++ src/main/java/io/getarrays/userservice/model/Role.java
@@ -0,0 +1,22 @@
+package io.getarrays.userservice.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String name;
+
+}
