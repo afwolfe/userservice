c267d87a76012863f0047654c91791ac2c4f0291
afwolfe <3718652+afwolfe@users.noreply.github.com>
2021-08-08 08:48:18 -0400

    JWT [1:33:24]

--- src/main/java/io/getarrays/userservice/filter/CustomAuthenticaionFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthenticaionFilter.java
@@ -1,6 +1,15 @@
 package io.getarrays.userservice.filter;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +19,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 public class CustomAuthenticaionFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final int ACCESS_EXPIRATION_TIME = 1000 * 60 * 10;
+  private final int REFRESH_EXPIRATION_TIME = ACCESS_EXPIRATION_TIME * 3;
 
   private AuthenticationManager authenticationManager;
 
@@ -32,7 +46,28 @@ public class CustomAuthenticaionFilter extends UsernamePasswordAuthenticationFil
 
   @Override
   protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-      FilterChain chain, Authentication authResult) throws IOException, ServletException {
+      FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    User user = (User) authentication.getPrincipal();
+    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+    String accessToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION_TIME))
+        .withIssuer(request.getRequestURL().toString())
+        .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(
+            Collectors.toList()))
+        .sign(algorithm);
+    String refreshToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION_TIME))
+        .withIssuer(request.getRequestURL().toString())
+        .sign(algorithm);
+    response.setHeader("access_token", accessToken);
+    response.setHeader("refresh_token", refreshToken);
+    Map<String, String> tokens = new HashMap<>();
+    tokens.put("access_token", accessToken);
+    tokens.put("refresh_token", refreshToken);
 
+    response.setContentType(APPLICATION_JSON_VALUE);
+    new ObjectMapper().writeValue(response.getOutputStream(), tokens);
   }
 }
--- src/main/java/io/getarrays/userservice/security/SecurityConfig.java
+++ src/main/java/io/getarrays/userservice/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package io.getarrays.userservice.security;
 
+import static io.getarrays.userservice.model.RoleEnum.ROLE_ADMIN;
+import static io.getarrays.userservice.model.RoleEnum.ROLE_USER;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
 import io.getarrays.userservice.filter.CustomAuthenticaionFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -34,9 +39,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    CustomAuthenticaionFilter customAuthenticaionFilter = new CustomAuthenticaionFilter(authenticationManagerBean());
+    customAuthenticaionFilter.setFilterProcessesUrl("/api/login");
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    http.authorizeRequests().anyRequest().permitAll();
-    http.addFilter(new CustomAuthenticaionFilter(authenticationManagerBean()));
+    http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+    http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority(ROLE_USER.toString());
+    http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority(ROLE_ADMIN.toString());
+    http.authorizeRequests().anyRequest().authenticated();
+    http.addFilter(customAuthenticaionFilter);
   }
 }
